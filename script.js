7709a0b2b132d43b030bc85ab23e735dcde8f5b7
Agnese Spinella <agnesespinella@gmail.com>
2022-08-22 17:19:45 +0200

    api url corrected

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let weather = {
-    "apiKey":"e619349c68f566ad88cfb2e877d09e43",
-    fetchWeather: function() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=e619349c68f566ad88cfb2e877d09e43")
+    apiKey:"e619349c68f566ad88cfb2e877d09e43",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ this.apiKey
+        )
         .then((response) => response.json())
         .then((data)=> console.log(data));
+    },
+    displayWeather: function(data) {
+
     }
 }
