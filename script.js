6fe2e0e2252ece6537ddb75c6be8f49d714bc60a
Agnese Spinella <agnesespinella@gmail.com>
2022-09-02 20:46:45 +0200

    data added

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ let weather = {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ this.apiKey
         )
         .then((response) => response.json())
-        .then((data)=> console.log(data));
+        .then((data)=> this.displayWeather(data));
     },
-    displayWeather: function(data) {
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } =  data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Meteo a " + name;
+        document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "K";
+        document.querySelector(".humidity").innerText = "Umidità: " + humidity + "%";
+        document.querySelector(".speed").innerText = "Velocità del vento: " + speed + "km/h";
 
     }
 }
